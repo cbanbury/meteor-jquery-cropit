e06fd82315a646b3d2cae00ca2a4c925a0a2614d
Carl Banbury <carl@googolmail.co.uk>
2015-09-24 12:36:31 +0100

    pulling cropit from submodule instead of hardcoded

--- package.js
+++ package.js
@@ -13,5 +13,5 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
     api.use('jquery', 'client');
-    api.addFiles('jquery.cropit.js', 'client');
+    api.addFiles('cropit/src/cropit.js', 'client');
 });
